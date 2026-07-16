04c07d1c8b8623bafbcd1f97b777a16be8521601
hongjin huang <hongjin.huang@sunmi.com>
2026-07-16 16:41:20 +0800

    sub branch

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-  int result = add(3, 4);
+  int result = add(3, -4);
   vector<int> numbers = {1, 2, 3, 4, 5};
   std::cout << "result: " << result << endl;
   return 0;
