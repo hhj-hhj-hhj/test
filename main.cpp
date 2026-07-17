853761577c36ea04325ba374d57a8cd7a6ce7e04
hongjin huang <hongjin.huang@sunmi.com>
2026-07-17 10:05:37 +0800

    change

--- main.cpp
+++ main.cpp
@@ -8,5 +8,8 @@ int main() {
   int result = add(35, 45);
   vector<int> numbers = {1, 2, 3, 4, 5};
   std::cout << "result: " << result << endl;
+  for (const auto &num : numbers) {
+    std::cout << num << " ";
+  }
   return 0;
 }
