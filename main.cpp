0ee001bf653bba76bb8ca937847d1a60d487e4d1
hongjin huang <hongjin.huang@sunmi.com>
2026-07-17 10:09:57 +0800

    new

--- main.cpp
+++ main.cpp
@@ -7,8 +7,5 @@ using namespace std;
 int main() {
   int result = add(35, 45);
   std::cout << "result: " << result << endl;
-  for (const auto &num : numbers) {
-    std::cout << num << " ";
-  }
   return 0;
 }
